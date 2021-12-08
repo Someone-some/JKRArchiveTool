02c727c7954b2e6dd9f091c95fc8e08f9c8ca47a
Someone <edward.tye@hotmail.com>
2021-12-08 19:53:31 +0000

    yet another oops

--- Include/Yaz0.h
+++ Include/Yaz0.h
@@ -5,7 +5,6 @@
 
 namespace Yaz0 {
     u8* decomp(const std::string &, u32 *, bool);
-    // 
     u32 compSimple(u8 *, s32, s32, u32 *);
     u32 compAdvanced(u8 *, s32, s32, u32 *);
     void comp(const std::string &);
