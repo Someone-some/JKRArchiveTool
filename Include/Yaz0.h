ad108e0a201fff9be50991fac1f97ebef486e85c
Someone <edward.tye@hotmail.com>
2021-12-08 19:51:54 +0000

    oops

--- Include/Yaz0.h
+++ Include/Yaz0.h
@@ -5,7 +5,7 @@
 
 namespace Yaz0 {
     u8* decomp(const std::string &, u32 *, bool);
-    // My Yaz0 compression algo sucked so I stole it from Yaz0enc
+    // 
     u32 compSimple(u8 *, s32, s32, u32 *);
     u32 compAdvanced(u8 *, s32, s32, u32 *);
     void comp(const std::string &);
